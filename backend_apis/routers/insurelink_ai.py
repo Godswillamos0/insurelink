5d32ad0cd7d2f85482198378c508bd648d33437d
Godswill Amos <amosgodswill00@gmail.com>
2025-06-20 20:30:59 +0100

    Update insurelink_ai.py

--- backend_apis/routers/insurelink_ai.py
+++ backend_apis/routers/insurelink_ai.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-env_path = Path(__file__).resolve().parent / ".env"
-load_dotenv(dotenv_path=env_path)
+#env_path = Path(__file__).resolve().parent / ".env"
+#load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
