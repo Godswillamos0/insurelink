082b8a71524aa3662a0da612183583a5440c5cbb
Godswill Amos <amosgodswill00@gmail.com>
2025-07-23 04:55:33 +0100

    Update database.py

--- backend_apis/database.py
+++ backend_apis/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./data.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://insurelink_user:BNoIrADzZ6vOC8FKrYEPeokE4mfiQjY6@dpg-d20513emcj7s73ao6p00-a.oregon-postgres.render.com/insurelink'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
